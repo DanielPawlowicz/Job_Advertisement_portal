7c27e1670e9fab9572e62cd96ff2db1f2490c1e4
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2024-07-31 13:25:56 +0200

    jsx syntax repetition

--- react-crash-2024/src/App.jsx
+++ react-crash-2024/src/App.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
 
 const App = () => {
+
+  const name = 'John';
+  const names = ['Brad', 'Mary', 'Joe', 'Sara'];
+  let loggedIn = null;
+  // loggedIn = true;
+  const styles = {
+    color: 'red',
+    fontSize: '55px'
+  };
+
+  if(loggedIn){
+    return (<h1>Hello Member</h1>);
+  }
+
   return (
-    <div>App</div>
+    <>
+      <div className='text-5xl'>App</div>
+      <p style={{color: 'red', fontSize: '24px'}}>Hello {name} </p>
+      <ul style = {styles}>
+        { names.map((name, index) => (
+          <li key={index}>{name}</li>
+        ))}
+      </ul>
+    </>
   )
 }
 
