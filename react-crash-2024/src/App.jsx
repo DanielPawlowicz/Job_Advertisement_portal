cd4528ea266ff0f8e71e3ff2ad151b34078329c5
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2024-07-31 13:27:53 +0200

    Clean setup

--- react-crash-2024/src/App.jsx
+++ react-crash-2024/src/App.jsx
@@ -1,31 +1,9 @@
 import React from 'react'
 
 const App = () => {
-
-  const name = 'John';
-  const names = ['Brad', 'Mary', 'Joe', 'Sara'];
-  let loggedIn = null;
-  // loggedIn = true;
-  const styles = {
-    color: 'red',
-    fontSize: '55px'
-  };
-
-  if(loggedIn){
-    return (<h1>Hello Member</h1>);
-  }
-
   return (
-    <>
-      <div className='text-5xl'>App</div>
-      <p style={{color: 'red', fontSize: '24px'}}>Hello {name} </p>
-      <ul style = {styles}>
-        { names.map((name, index) => (
-          <li key={index}>{name}</li>
-        ))}
-      </ul>
-    </>
+    <div>App</div>
   )
 }
 
-export default App;
+export default App
