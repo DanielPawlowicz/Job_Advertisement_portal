755977e30796b037c92d87c26a49e12e53783e8e
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2024-08-02 13:57:53 +0200

    Default props in Hero

--- react-crash-2024/src/components/Hero.jsx
+++ react-crash-2024/src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Hero = ({title, subtitle}) => {
+const Hero = ({title = 'Become a React Dev', subtitle = 'Find the React job that fits your skill set'}) => {
   return (
     <>
         <section className="bg-indigo-700 py-20 mb-4">
