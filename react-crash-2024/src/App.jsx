d024711ac5395f9dcaddb0373786fae7ea08eb99
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2024-08-12 14:30:49 +0200

    Router installation and import

--- react-crash-2024/src/App.jsx
+++ react-crash-2024/src/App.jsx
@@ -1,9 +1,12 @@
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom'
 import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import HomeCards from './components/HomeCards'
 import JobListings from './components/JobListings'
 import ViewAllJobs from './ViewAllJobs'
+
+
 
 const App = () => {
   return (
