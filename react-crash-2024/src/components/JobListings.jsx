f0347df44e6c13382983d22c08ea7eb78dae07cf
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2024-08-30 16:51:09 +0200

    Universal apiUrl so jobListings show 3 or all jobs

--- react-crash-2024/src/components/JobListings.jsx
+++ react-crash-2024/src/components/JobListings.jsx
@@ -14,9 +14,12 @@ const JobListings = ({ isHome = false}) => {
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
+
+      const apiUrl = isHome ? 'http://localhost:8000/jobs?_limit=3' : 'http://localhost:8000/jobs'
+
       const fetchJobs = async () => {
         try{
-          const res = await fetch('http://localhost:8000/jobs');
+          const res = await fetch(apiUrl);
           const data = await res.json();
           setJobs(data);
         } catch(error) {
@@ -36,7 +39,7 @@ const JobListings = ({ isHome = false}) => {
         <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
           { isHome ? 'Recent Jobs' : 'Browse Jobs'}
         </h2>
-        
+
           { loading 
             ? (<Spinner loading/>) 
             : (<div className="grid grid-cols-1 md:grid-cols-3 gap-6">
