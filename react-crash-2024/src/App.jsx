f8c84bb8bb978359d6bdd63da561ce858e82810b
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2024-09-03 14:19:01 +0200

    Deleting job request

--- react-crash-2024/src/App.jsx
+++ react-crash-2024/src/App.jsx
@@ -23,7 +23,10 @@ const App = () => {
 
   // Delete job
   const deleteJob = async (id) =>{
-    console.log('delete', id);
+    const res = await fetch(`/api/jobs/${id}`, {
+      method: 'DELETE',
+    });
+    return;
   };
   
   const router = createBrowserRouter(
